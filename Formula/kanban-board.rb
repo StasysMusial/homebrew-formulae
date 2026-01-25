806092ee50d9775275a6d9ec151e092af01bc12f
StasysMusial <oderauchnicht@me.com>
2026-01-25 05:54:37 +0100

    added proper checksum

--- Formula/kanban-board.rb
+++ Formula/kanban-board.rb
@@ -3,7 +3,7 @@ class KanbanBoard < Formula
   homepage "https://github.com/StasysMusial/kanban-board"
   url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.0.1.tar.gz"
   version "1.0.1"
-  sha256 ""
+  sha256 "94b221bd77b05ede43173becb6995c2019150202535528bc8acf703a52226e0b"
   license "MIT"
 
   depends_on "go" => :build
