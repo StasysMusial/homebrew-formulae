3e884ce7ccc158d0b067cf0e3c8b623ad5ef81b8
StasysMusial <62400912+StasysMusial@users.noreply.github.com>
2026-01-25 14:08:19 +0100

    Fix version command in kanban-board.rb

--- Formula/kanban-board.rb
+++ Formula/kanban-board.rb
@@ -2,7 +2,6 @@ class KanbanBoard < Formula
   desc "Minimal kanban board TUI"
   homepage "https://github.com/StasysMusial/kanban-board"
   url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.0.1.tar.gz"
-  version "1.0.1"
   sha256 "94b221bd77b05ede43173becb6995c2019150202535528bc8acf703a52226e0b"
   license "MIT"
 
@@ -13,6 +12,6 @@ class KanbanBoard < Formula
   end
 
   test do
-    system "kanban-board", "--version"
+    system bin "kanban-board", "--version"
   end
 end
