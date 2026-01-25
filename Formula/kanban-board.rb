8e695bb5fa13cccaaa1d5f24b970e8344b712b27
StasysMusial <oderauchnicht@me.com>
2026-01-25 05:02:45 +0100

    added install and test

---
+++ Formula/kanban-board.rb
@@ -0,0 +1,27 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class KanbanBoard < Formula
+  desc "a minimal kanban board TUI"
+  homepage "https://github.com/StasysMusial/kanban-board"
+  url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "993c61f600d65a7e8e2b86f1cf1fd8cd1364e4e6dc854b51516d878616c03a19"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "#{bin}/kanban-board", "--version"
+  end
+end
