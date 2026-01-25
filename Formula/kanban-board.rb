4beec755548ff2227b22b3d873083d737f52176a
StasysMusial <oderauchnicht@me.com>
2026-01-25 05:46:50 +0100

    removed checksum

--- Formula/kanban-board.rb
+++ Formula/kanban-board.rb
@@ -3,7 +3,7 @@ class KanbanBoard < Formula
   homepage "https://github.com/StasysMusial/kanban-board"
   url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.0.1.tar.gz"
   version "1.0.1"
-  sha256 "d06d8296f62a3bf3748836a040e04d0a1f3434fc8501d57080cf76b352bf2308"
+  sha256 ""
   license "MIT"
 
   depends_on "go" => :build
