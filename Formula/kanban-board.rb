2a3089c279b5314f3682dc87d930b06417aac7b6
StasysMusial <oderauchnicht@me.com>
2026-01-27 20:05:36 +0100

    v1.1.0

--- Formula/kanban-board.rb
+++ Formula/kanban-board.rb
@@ -1,8 +1,8 @@
 class KanbanBoard < Formula
   desc "Minimal kanban board TUI"
   homepage "https://github.com/StasysMusial/kanban-board"
-  url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "94b221bd77b05ede43173becb6995c2019150202535528bc8acf703a52226e0b"
+  url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "E217323A76667C1E527611565D6FC8E834389EFCF6B302A2C347210788BA65B6"
   license "MIT"
 
   depends_on "go" => :build
