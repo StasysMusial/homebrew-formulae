b01ad2633243584f443332ad1eb57930d65021e0
StasysMusial <oderauchnicht@me.com>
2026-01-28 04:30:27 +0100

    v1.1.2: better argument handling
    more in line with other cli apps

--- Formula/kanban-board.rb
+++ Formula/kanban-board.rb
@@ -1,8 +1,8 @@
 class KanbanBoard < Formula
   desc "Minimal kanban board TUI"
   homepage "https://github.com/StasysMusial/kanban-board"
-  url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "ee5bcd126be3bb7f0a78fd2552ee02061093444a99d8bf28cc9996d61ef427cf"
+  url "https://github.com/StasysMusial/kanban-board/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "6cca4ca24b5327ca06a84c0abe27e4bdbe4171a48224d3f92c2f1005e57d1c60"
   license "MIT"
 
   depends_on "go" => :build
